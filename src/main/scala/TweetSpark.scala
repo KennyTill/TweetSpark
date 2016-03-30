4645941ac41460d636b639d8f970ff37ee55e586
Kenny Till <kenneth.g.till@gmail.com>
2016-03-30 14:05:53 -0700

    adding in helper call for spark setup

--- src/main/scala/TweetSpark.scala
+++ src/main/scala/TweetSpark.scala
@@ -3,15 +3,16 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.twitter._
 import org.apache.spark.streaming.StreamingContext._
-
-
+import HelperMethods._
 /**
   * Created on 3/29/16.
   */
 object TweetSpark {
   def main(args: Array[String]): Unit = {
-    //need spark url
-    val sparkConfig = new SparkConf().setMaster("").setAppName("TwitterSpark")
+
+    //need spark url for master of cluster
+    val sparkUrl = HelperMethods.getSparkURL()
+    val sparkConfig = new SparkConf().setMaster(sparkUrl).setAppName("TwitterSpark")
     val scc = new StreamingContext(sparkConfig, Seconds(10))
     val stream = TwitterUtils.createStream(scc, None)
 
